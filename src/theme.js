b5a8c10f00919d2753032c76fb6f69c70af45ea5
Praju2002 <104666507+Praju2002@users.noreply.github.com>
2024-02-12 13:06:10 +0545

    Navbar added

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f3ecf1",
+    },
+    secondary: {
+      main: "#743d72",
+      secondary_300: "#eeeeee",
+      secondary_400: "#480765",
+      secondary_600: "#6a1067",
+    },
+    tertiary: {
+      main: "#f58220",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 950,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  typography: {
+    fontFamily: "Roboto Serif",
+  },
+});
+
+export default theme;
